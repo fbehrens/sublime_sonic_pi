93e5bcb201aab6df109506587bd50221dbf5e5c2
Frank Behrens <fbehrens@gmail.com>
2015-08-17 21:57:24 +0200

    init

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Synopsis
+This plugin can command sonic Pi from sublime. It requires ruby and uses 
+[osc-ruby](https://rubygems.org/gems/osc-ruby) and [sonic-pi-cli](https://rubygems.org/gems/sonic-pi-cli), 
+which is a thin commandline wrapper for above.
+
+It works for me on
+* OSX 10.10.5 
+* Sublime 3 Build 3083
+* Sonic Pi 2.6
+* ruby 2.1.5p273
+
+# more Requirements 
+Install _sonic-pi-cli_ gem with  `gem install sonic-pi-cli`
+_Sonic Pi_ Must be running. 
+So `sonic_pi play 40` should be working. 
+
+# Hotkeys
+`Alt-r`      run current file
+`Alt-Enter`  run current selection
+`Alt-s`      stop playing 
---
+++ sonic_pi.py
@@ -0,0 +1,34 @@
+import sublime, sublime_plugin
+from subprocess import Popen
+
+def sonic_pi(command):
+    print("play:" + command )
+    Popen("sonic_pi \""+command.replace('"','\\"')+"\"",shell=True)
+
+class SonicPiRun(sublime_plugin.TextCommand):
+    def run(self, edit):
+        print("run")
+        r = sublime.Region(0,self.view.size())
+        s = self.view.substr(r)
+        sonic_pi(s)
+
+class SonicPiStop(sublime_plugin.TextCommand):
+    def run(self, edit):
+        print("stop")
+        sonic_pi("stop")
+
+class SonicPiSelection(sublime_plugin.TextCommand):
+    def line(self):
+        for region in self.view.sel():
+            if region.empty():
+                line = self.view.line(region)
+                line_contents = self.view.substr(line)
+                line_below = sublime.Region(line.b+1)
+                self.view.sel().clear()
+                self.view.sel().add(line_below)
+                return line_contents
+            else:
+                return self.view.substr(region)
+    def run(self, edit):
+        print("selection")
+        sonic_pi(self.line())
